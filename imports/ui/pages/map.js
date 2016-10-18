808128f8330881953e4a8443dc107cd058c53a87
Nick West <mail@nickwe.st>
2016-10-18 00:19:25 -0400

    es6-ified google map

--- imports/ui/pages/map.js
+++ imports/ui/pages/map.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render } from 'react-dom';
 import {Link} from 'react-router';
 import {
     Row,
@@ -24,11 +25,76 @@ export class Maps extends React.Component {
                 lat: 40.7128,
                 lng: -74.0059
             },
+            zoom: 10,
             showModal: false,
         };
         this.close = this.close.bind(this);
         this.open = this.open.bind(this);
     }
+
+    render() {
+      return <div className="GMap">
+        <div className='UpdatedText'>
+          <p>Current Zoom: { this.state.zoom }</p>
+        </div>
+        <div className='GMap-canvas' ref="mapCanvas">
+        </div>
+      </div>
+    }
+
+    componentDidMount() {
+      // create the map, marker and infoWindow after the component has
+      // been rendered because we need to manipulate the DOM for Google =(
+      this.map = this.createMap()
+      this.marker = this.createMarker()
+      this.infoWindow = this.createInfoWindow()
+
+      // have to define google maps event listeners here too
+      // because we can't add listeners on the map until its created
+      google.maps.event.addListener(this.map, 'zoom_changed', ()=> this.handleZoomChange())
+    }
+
+    componentDidUnMount() {
+      google.maps.event.clearListeners(map, 'zoom_changed')
+    }
+
+    createMap() {
+      let mapOptions = {
+        zoom: this.state.zoom,
+        center: this.mapCenter()
+      }
+      return new google.maps.Map(this.refs.mapCanvas, mapOptions)
+    }
+
+    mapCenter() {
+      return new google.maps.LatLng(
+        this.state.defaultCenter.lat,
+        this.state.defaultCenter.lng
+      )
+    }
+
+    createMarker() {
+      return new google.maps.Marker({
+        position: this.mapCenter(),
+        map: this.map
+      })
+    }
+
+    createInfoWindow() {
+      let contentString = "<div class='InfoWindow'>I'm a Window that contains Info Yay</div>"
+      return new google.maps.InfoWindow({
+        map: this.map,
+        anchor: this.marker,
+        content: contentString
+      })
+    }
+
+    handleZoomChange() {
+      this.setState({
+        zoom: this.map.getZoom()
+      })
+    }
+
 
     close() {
         this.setState({ showModal: false });
@@ -38,92 +104,4 @@ export class Maps extends React.Component {
         this.setState({ showModal: true });
       }
 
-    componentDidMount() {
-        // this.getCurrentPosition();
-        var contentString = '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">New York</h1>'+
-            '<div id="bodyContent">'+
-            '<p>content string</p>'+
-            '</div>'+
-            '</div>';
-
-        let sourland = {lat: 40.473927, lng: -74.694482};
-
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: {
-                lat: 40.7128,
-                lng: -74.0059
-            }
-        });
-
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString,
-          maxWidth: 200
-        });
-
-        var marker = new google.maps.Marker({
-          position: sourland,
-          map: map,
-          title: 'Uluru (Ayers Rock)'
-        });
-
-        // modal code
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-          this.open;
-        });
-    }
-
-
-    render(){
-        console.log(this.state);
-
-        const popover = (
-          <Popover id="modal-popover" title="popover">
-            very popover. such engagement
-          </Popover>
-        );
-        const tooltip = (
-          <Tooltip id="modal-tooltip">
-            wow.
-          </Tooltip>
-        );
-
-        return (
-            <div>
-                <div id="map" className="map-container"></div>
-
-                <Button
-                  bsStyle="primary"
-                  bsSize="large"
-                  onClick={this.open}
-                >
-                  Launch demo modal
-                </Button>
-
-                <Modal show={this.state.showModal} onHide={this.close}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Sourland Mountain Preserve Climbs</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    <h4>Sourland Mountain Preserve</h4>
-                    <p>Climbing is allowed in blank, but watch out for blank.</p>
-
-                    <h4>This is a Popover</h4>
-                    <p>there is a <OverlayTrigger overlay={popover}><a href="#">popover</a></OverlayTrigger> here</p>
-
-                    <h4>Tooltips in a modal</h4>
-                    <p>there is a <OverlayTrigger overlay={tooltip}><a href="#">tooltip</a></OverlayTrigger> here</p>
-                  </Modal.Body>
-
-                  <Modal.Footer>
-                    <Button onClick={this.close}>Close</Button>
-                  </Modal.Footer>
-                </Modal>
-            </div>
-        );
-    }
 }
